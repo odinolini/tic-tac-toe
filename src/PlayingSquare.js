87a1d8327eb705dcd82fb5eb36ff5ab08c550721
odinolini <aoandberg@gmail.com>
2018-09-02 14:47:39 +0200

    Added lines over the board when winning

--- src/PlayingSquare.js
+++ src/PlayingSquare.js
@@ -6,9 +6,22 @@ export default class PlayingSquare extends Component {
   
       let number = this.props.number;
       let fadeIn = this.props.board[number] === 1 || this.props.board[number] === 2 ? "fade-in" : "";
+      let winRow = this.props.number === this.props.winRow ? "rowline" : "";
+      let winCol = this.props.number === this.props.winCol ? "colline" : "";
+      //Number 4 is the center square, where the diag line will be drawn
+      let winDiag;
+
+      if (this.props.number === 4 && this.props.winDiag === "backline") {
+          winDiag = "backDiagLine";
+      } else if (this.props.number === 4 && this.props.winDiag === "fwd") {
+          winDiag = "fwdDiagLine";
+      }
+
+      let classNames = ["grid-item", winRow, winCol, winDiag];
+      
       
       return (
-        <div className={this.props.className} onClick={() => this.props.onClick(number)}>
+        <div className={classNames.join(" ")} onClick={() => this.props.onClick(number)}>
           <span className={fadeIn}>
           {this.props.board[number] === 1 ? "X" : ""}
           {this.props.board[number] === 2 ? "O" : ""}
--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,89 +1,90 @@
-import React, { Component } from 'react';
-import PlayingSquare from './PlayingSquare.js';
-import Winner from './Winner.js';
-import TurnDisplayer from './TurnDisplayer.js';
+import React, { Component } from "react";
+import PlayingSquare from "./PlayingSquare.js";
+import Winner from "./Winner.js";
+import TurnDisplayer from "./TurnDisplayer.js";
 
 export default class TicTacToe extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        board: [
-           0,0,0,
-           0,0,0,
-           0,0,0
-          ],
-        player: 1,
-        xSize: 3,
-        ySize: 3,
-        stage: 0,
-        gridClassNames: ["grid-container"]
-      };
+        super(props);
+        this.state = {
+            board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            player: 1,
+            xSize: 3,
+            ySize: 3,
+            stage: 0,
+            gridClassNames: ["grid-container"]
+        };
     }
-  
-  
-    handlePlayingSquareClick = (key) => {
-  
-      if (this.state.board[key] !== 0 || this.state.winner) {
-        //do nothing if it is checked or game is won
-        return;
-      }
-  
-      let newBoard = this.state.board.slice();
-  
-      switch(this.state.player) {
-        case 1:
-          newBoard[key] = 1;
-          this.winConditionCheck(newBoard, 1);
-          break;
-        case 2:
-          newBoard[key] = 2;
-          this.winConditionCheck(newBoard, 2);
-          break;
-        default: break;
-      }
-      
-  
-      this.setState({
-        board: newBoard,
-        player: this.state.player === 1 ? 2 : 1
-      });
 
-      
-  
-    }
-  
-    winConditionCheck(board, player) {
-      this.setState({stage: this.state.stage+1});
-      
-      if (this.checkRows(board, player) || this.checkCols(board, player) || this.checkDiagonals(board, player)) {
-        this.setState({winner: player, gridClassNames: ["grid-container", "grid-animation"]});
-      } else if (this.state.stage === (this.state.xSize * this.state.ySize)-1) {
-        this.setState({winner: "tie"});
-      }
-    }
-    
-    checkRows(board, player) {
-      for (let i=0; i < board.length; i+= this.state.xSize) {
-        if (board[i] === player) {
-          for (let x = 0; x < this.state.xSize; x++) {
-            if (board[i+x] !== player) {
-              return false;
-            }
-          }
-        } else if (board[i] !== player) {
-          continue;
+    handlePlayingSquareClick = key => {
+        if (this.state.board[key] !== 0 || this.state.winner) {
+            //do nothing if it is checked or game is won
+            return;
         }
-        console.log("Row: ", i);
-        return true;
-      }
+
+        let newBoard = this.state.board.slice();
+
+        switch (this.state.player) {
+            case 1:
+                newBoard[key] = 1;
+                this.winConditionCheck(newBoard, 1);
+                break;
+            case 2:
+                newBoard[key] = 2;
+                this.winConditionCheck(newBoard, 2);
+                break;
+            default:
+                break;
+        }
+
+        this.setState({
+            board: newBoard,
+            player: this.state.player === 1 ? 2 : 1
+        });
+    };
+
+    winConditionCheck(board, player) {
+        this.setState({ stage: this.state.stage + 1 });
+
+        if (
+            this.checkRows(board, player) ||
+            this.checkCols(board, player) ||
+            this.checkDiagonals(board, player)
+        ) {
+            this.setState({
+                winner: player,
+                gridClassNames: ["grid-container", "grid-animation"]
+            });
+        } else if (
+            this.state.stage ===
+            this.state.xSize * this.state.ySize - 1
+        ) {
+            this.setState({ winner: "tie" });
+        }
     }
-  
-    
-    checkCols(board, player) {
-        for (let i=0; i < this.state.ySize; i++) {
+
+    checkRows(board, player) {
+        for (let i = 0; i < board.length; i += this.state.xSize) {
             if (board[i] === player) {
-                for (let x=0; x < board.length; x+= this.state.ySize) {
-                    if (board[i+x] !== player) {
+                for (let x = 0; x < this.state.xSize; x++) {
+                    if (board[i + x] !== player) {
+                        return false;
+                    }
+                }
+            } else if (board[i] !== player) {
+                continue;
+            }
+            console.log("Row: ", i);
+            this.setState({ winRow: i });
+            return true;
+        }
+    }
+
+    checkCols(board, player) {
+        for (let i = 0; i < this.state.ySize; i++) {
+            if (board[i] === player) {
+                for (let x = 0; x < board.length; x += this.state.ySize) {
+                    if (board[i + x] !== player) {
                         return false;
                     }
                 }
@@ -91,58 +92,75 @@ export default class TicTacToe extends Component {
                 continue;
             }
             console.log("Col: ", i);
+            //+3 so it selects one of the middle squares
+            //this is so the line displays correctly
+            this.setState({ winCol: i + 3 });
             return true;
         }
     }
-    
+
     checkDiagonals(board, player) {
-      //to do: scalable diagonal check
-      if ((
-        board[0] === player &&
-        board[4] === player &&
-        board[8] === player )||(
-        board[2] === player &&
-        board[4] === player &&
-        board[6] === player
-      )) {
-        return true;
-      }
+        //to do: scalable diagonal check
+        if (board[0] === player && board[4] === player && board[8] === player) {
+          this.setState({winDiag: "backline"});
+          return true;
+        } else if (
+          board[2] === player && board[4] === player && board[6] === player) {
+            this.setState({winDiag: "fwd"});
+            return true;
+        }
         return false;
     }
 
     restart = () => {
-        this.setState({board: [0,0,0,0,0,0,0,0,0], player: 1, winner: null, stage: 0});
-    }
-
+        this.setState({
+            board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            player: 1,
+            stage: 0,
+            winner: null,
+            winRow: null,
+            winCol: null,
+            winDiag: null
+        });
+    };
 
     render() {
-      const boardSize = this.state.xSize * this.state.ySize;
-      let playingSquares = [];
-  
-      for (let i = 0; i < boardSize; i++) {
-        playingSquares.push(
-          <PlayingSquare 
-          className="grid-item"
-          onClick={this.handlePlayingSquareClick}
-          player={this.state.player}
-          key={i}
-          number={i}
-          board={this.state.board}
-          />);
-      }
+        const boardSize = this.state.xSize * this.state.ySize;
+        let playingSquares = [];
 
-  
-      return (
-          <React.Fragment>
-          {this.state.winner ? "" : <TurnDisplayer player={this.state.player} />}
+        for (let i = 0; i < boardSize; i++) {
+            playingSquares.push(
+                <PlayingSquare
+                    onClick={this.handlePlayingSquareClick}
+                    player={this.state.player}
+                    key={i}
+                    number={i}
+                    board={this.state.board}
+                    winRow={this.state.winRow}
+                    winCol={this.state.winCol}
+                    winDiag={this.state.winDiag}
+                />
+            );
+        }
 
-          {this.state.winner ? <Winner onClick={this.restart} player={this.state.winner} /> : ""}
-            
-          {/* !this.state.winner ? <div className="grid-container">{playingSquares}</div> : "" */}
-          <div className={this.state.gridClassNames.join(" ")} >{playingSquares}</div>
-            
-          </React.Fragment>
-      );
-      
+        return (
+            <React.Fragment>
+                {this.state.winner ? (
+                    ""
+                ) : (
+                    <TurnDisplayer player={this.state.player} />
+                )}
+
+                {this.state.winner ? (
+                    <Winner onClick={this.restart} player={this.state.winner} />
+                ) : (
+                    ""
+                )}
+
+                <div className={this.state.gridClassNames.join(" ")}>
+                    {playingSquares}
+                </div>
+            </React.Fragment>
+        );
     }
-  }
+}
